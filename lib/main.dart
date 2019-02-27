8b3daf1067440b233d583fe7ab14b325a609d191
Omar <omarh.ismail1@gmail.com>
2019-02-27 22:59:28 +0200

    Removed Toast Plugin

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
